7fe86b3087050e5d1874ff66be71eb40f6061434
Rebecka <rebecka.rl.larsson@gmail.com>
2025-01-24 12:52:57 +0100

    3rd yet to fix product details, cbr failed for now

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router'
 
 import RootLayouts from './layouts/RootLayouts'
 import Home from './pages/Home'
@@ -23,42 +23,71 @@ import { ProductsProvider } from './contexts/ProductsContext'
 import ProductList from './pages/ProductList'
 import ProductDetails from './components/ProductDetails'
 
+const router = createBrowserRouter(
+  {
+    element: <RootLayouts />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'productList',
+        element: <ProductList />
+      },
+      {
+        path: 'productList/:productId',
+        element: <ProductDetails />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />
+      },
+      {
+        path: '*',
+        element: <NotFound />
+      },
+    ]
+  },
+  {
+    element: <LoginLayouts />,
+    children: [
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
+      },
+    ]
+  },
+  {
+    element: <LoggedInLayouts />,
+    children: [
+      {
+        path: 'orderhistory',
+        element: <OrderHistory />
+      },
+    ]
+  },
+  {
+    element: <Footer />
+  }
+)
+
 // måsvingar för att kunna skriva JS/element
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ProductsProvider>
-    <BrowserRouter>
-        {/* routes är som en switch, hette switch, 
-        tittar på url och ge nngt av elementen såsom 
-        home faq osv. */}
-        
-      <Routes> 
-            {/* vad har den för path, alltså hur ska url se ut för
-            att den ska vara aktiv, */}
-        <Route element={ <RootLayouts /> }>
-          <Route path ='/' element={ <Home /> } />
-          {/* <Route path ='/products' element={ <Products /> } />
-          <Route path ='/products/:productItem' element={ <ProductItem /> } /> */}
-          <Route path ='/productList' element={ <ProductList /> } />
-          <Route path ='/productList/:productId' element={ <ProductDetails /> } />
-          <Route path ='/contact' element={ <Contact /> } />
-          <Route path ='/checkout' element={ <Checkout /> } />
-          <Route path='*' element={ <NotFound />}/>
-        </Route>
-
-        <Route element={ <LoginLayouts /> }>
-          <Route path='/login' element={ <Login /> } />
-          <Route path='/register' element={ <Register /> } />
-        </Route>
-
-        <Route element={ <LoggedInLayouts /> }>
-          <Route path ='/orderhistory' element={ <OrderHistory /> } />
-        </Route>
-
-      </Routes>
-      <Footer />
-
-    </BrowserRouter>
+    {/* <BrowserRouter> */}
+      <RouterProvider router={router}/>
+      {/* <Footer /> */}
+    {/* </BrowserRouter> */}
     </ProductsProvider>
   </StrictMode>
 )
