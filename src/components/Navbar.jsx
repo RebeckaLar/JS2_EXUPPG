4156153a820b63d4e2a1dbdc63c9cf2a0c87fcc1
Rebecka <rebecka.rl.larsson@gmail.com>
2025-01-29 16:35:25 +0100

    cart added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, NavLink } from 'react-router'
 import Logo from '../Placeholders/Logo.svg';
+import { IoMdCart } from "react-icons/io";
 
 const Navbar = () => {
   return (
@@ -8,12 +9,12 @@ const Navbar = () => {
             <li><Link to="/" className='nav-link'>
             <img src={Logo}></img>
             </Link></li>
-            <ul className='nav-links'>
+            <ul className='nav-links align-center'>
                 <li><NavLink to="/" className='nav-link'>Home</NavLink></li>
                 <li><NavLink to="/productList" className='nav-link'>Products</NavLink></li>
                 <li><NavLink to="/contact" className='nav-link'>Contact</NavLink></li>
                 <li><NavLink to="/login" className='nav-link'>Login</NavLink></li>
-                <li><NavLink to="/checkout" className='nav-link'>C</NavLink></li>
+                <li><NavLink to="/checkout" className='nav-link'><IoMdCart /></NavLink></li>
             </ul>
         </div>
     </div>
